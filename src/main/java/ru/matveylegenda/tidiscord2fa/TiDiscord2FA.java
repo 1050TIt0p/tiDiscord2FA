72f03c7c46e369d7c4ea10ae94688732463adcba
1050TIt0p <160386710+1050TIt0p@users.noreply.github.com>
2025-01-14 16:00:03 +0300

    First commit

---
+++ src/main/java/ru/matveylegenda/tidiscord2fa/TiDiscord2FA.java
@@ -0,0 +1,17 @@
+package ru.matveylegenda.tidiscord2fa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TiDiscord2FA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
